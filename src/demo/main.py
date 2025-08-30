286e6be5ae3a3158169d927cfcd17dcf4c4ae246
Abhibhav Sharma <139039304+Abhibhav2003@users.noreply.github.com>
2025-08-30 16:41:12 +0530

    Complete Multi-modal Attendance System

---
+++ src/demo/main.py
@@ -0,0 +1,8 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from website import create_app
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
